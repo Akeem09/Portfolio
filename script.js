8209c2720aca4989782ad3d64f9a8679c4d48853
SHAIK AKEEM <138660764+Akeem09@users.noreply.github.com>
2023-08-20 16:20:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+var akeem = new Typed(".textttt",{
+    strings:["Frontend Developer.", "Backend Developer.", "Web-Designer.", "Student."],
+    typeSpeed: 30,
+    backSpeed: 30,
+    backDelay: 1000,
+    loop:true
+});
+
+
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000);
+}
